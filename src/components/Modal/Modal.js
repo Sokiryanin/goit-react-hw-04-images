a495db673ca197b2dbe1d84a753e5282233ca926
Sokiryanin <eugene.sidorov@me.com>
2023-09-01 22:02:35 +0200

    styled modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,7 +4,23 @@ ReactModal.setAppElement('#root');
 
 export const ModalImage = ({ url, name, modalOpen, modalClose }) => {
   return (
-    <ReactModal isOpen={modalOpen} onRequestClose={() => modalClose()}>
+    <ReactModal
+      isOpen={modalOpen}
+      onRequestClose={() => modalClose()}
+      style={{
+        overlay: {},
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          width: '50vw',
+          padding: '0',
+        },
+      }}
+    >
       <img src={url} alt={name} />
     </ReactModal>
   );
